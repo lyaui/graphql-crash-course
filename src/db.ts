6bd01fd66552e94508ce85aebe0095b10092d7c5
Iris Huang <lyaui@outlook.com>
2024-02-15 03:17:00 +0800

    feat: add resolvers

--- src/db.ts
+++ src/db.ts
@@ -1,33 +1,3 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-
-// import db from './db';
-// import { typeDefs } from './schema';
-
-const typeDefs = `#graphql
-  type Game {
-    id: ID!
-    title: String!
-    platform: [String!]!
-  }
-  type Review {
-    id: ID!
-    rating: Int!
-    content: String!
-  }
-  type Author {
-    id: ID!
-    name: String!
-    verified: Boolean!
-  }
-  # must to have
-  type Query {
-    reviews:[Review]
-    games:[Game]
-    authors:[Author]
-  }
-`;
-
 let games = [
   { id: '1', title: 'Zelda, Tears of the Kingdom', platform: ['Switch'] },
   { id: '2', title: 'Final Fantasy 7 Remake', platform: ['PS5', 'Xbox'] },
@@ -52,33 +22,4 @@ let reviews = [
   { id: '7', rating: 10, content: 'lorem ipsum', author_id: '3', game_id: '1' },
 ];
 
-const db = { games, authors, reviews };
-
-const resolvers = {
-  Query: {
-    games() {
-      return db.games;
-    },
-    reviews() {
-      return db.reviews;
-    },
-    authors() {
-      return db.authors;
-    },
-  },
-};
-
-const server = new ApolloServer({
-  // typeDef -- definitions of typws of data
-  typeDefs,
-  // resolvers (how to respond to queries with different data)
-  resolvers,
-});
-
-const { url } = await startStandaloneServer(server, {
-  listen: {
-    port: 4000,
-  },
-});
-
-console.log('Server ready at port', 4000);
+export default { games, authors, reviews };
